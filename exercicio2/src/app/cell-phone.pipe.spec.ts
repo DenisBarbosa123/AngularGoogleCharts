f7d62ba151f315463be1a850884f88cc29aa8711
Denis Barbosa <denis_barbosa123@hotmail.com>
2019-11-14 22:25:00 -0300

    removendo arquivos desnecessarios

--- exercicio2/src/app/cell-phone.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CellPhonePipe } from './cell-phone.pipe';
-
-describe('CellPhonePipe', () => {
-  it('create an instance', () => {
-    const pipe = new CellPhonePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
